84b0963c4be915c7fb56d895094141d73c251234
askhat abishev <asky.gm@gmail.com>
2018-07-29 15:30:01 +0600

    Initial commit

---
+++ src/main/java/kz/abishev/askhat/itbrainworkout/ItBrainWorkoutApplication.java
@@ -0,0 +1,12 @@
+package kz.abishev.askhat.itbrainworkout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItBrainWorkoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItBrainWorkoutApplication.class, args);
+	}
+}
